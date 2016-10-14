ef653e6dda02269a9e86f4463284d357a1007ced
Tzu-Ching Wu <wu509@purdue.edu>
2016-10-14 13:09:49 -0400

    Add files via upload

--- functions/parameter_setting.m
+++ functions/parameter_setting.m
@@ -22,7 +22,7 @@ function varargout = parameter_setting(varargin)
 
 % Edit the above text to modify the response to help parameter_setting
 
-% Last Modified by GUIDE v2.5 01-Sep-2016 21:45:30
+% Last Modified by GUIDE v2.5 14-Oct-2016 11:36:51
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -54,15 +54,45 @@ function parameter_setting_OpeningFcn(hObject, eventdata, handles, varargin)
 % varargin   command line arguments to parameter_setting (see VARARGIN)
 
 % Choose default command line output for parameter_setting
+% Choose default command line output for eyesmFISH_GUI
 handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
 
 % Update handles structure
 load(['/functions/' 'root.mat']);
 load([datafolder 'data.mat']);
 handles.datafolder=datafolder;
-p=p_original;
- 
-save([datafolder 'data.mat'],'p','-append');
+
+% load p parameters from data.mat in datafolder
+set(handles.checkbox1,'Value',p.savetiff);
+set(handles.checkbox2,'Value',p.savefinalresult);
+set(handles.checkbox9,'Value',p.rawImage16);
+set(handles.checkbox8,'Value',p.closeallfigure);
+set(handles.popupmenu3,'Value',p.imageformat);
+set(handles.edit6,'String',num2str(p.DAPI_chal));
+set(handles.edit7,'String',num2str(p.FISH_chal));
+if strcmp(p.gau_type,'gaussian')==1
+    set(handles.popupmenu1,'Value',1);
+else
+    set(handles.popupmenu1,'Value',1);
+end
+set(handles.edit3,'String',num2str(p.gau_hsize));
+set(handles.edit4,'String',num2str(p.gau_sigma));
+set(handles.edit8,'String',num2str(p.init_stack));
+set(handles.edit9,'String',num2str(p.final_stack));
+set(handles.edit2,'String',num2str(p.back_th_num));
+set(handles.edit5,'String',num2str(p.back_back_erode));
+set(handles.edit10,'String',num2str(p.back_back_open));
+set(handles.edit11,'String',num2str(p.back_nuc_open));
+set(handles.edit12,'String',num2str(p.back_nuc_peakthresh));
+set(handles.edit13,'String',num2str(p.candi_start));
+set(handles.edit14,'String',num2str(p.candi_end));
+set(handles.edit15,'String',num2str(p.nearby_radius_max));
+set(handles.edit16,'String',num2str(p.remove_bou_back));
+set(handles.edit17,'String',num2str(p.dist_radius_ratio));
+
 guidata(hObject, handles);
 
 % UIWAIT makes parameter_setting wait for user response (see UIRESUME)
@@ -77,7 +107,145 @@ function varargout = parameter_setting_OutputFcn(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Get default command line output from handles structure
-varargout{1} = handles.output;
+%varargout{1} = handles.output;
+
+% Update handles structure
+
+% Update handles structure
+guidata(hObject, handles);
+
+% Update handles structure
+load(['/functions/' 'root.mat']);
+load([datafolder 'data.mat']);
+%{
+load(['/functions/' 'root.mat']);
+
+p.savetiff=get(handles.checkbox1,'Value');
+p.savefinalresult=get(handles.checkbox2,'Value');
+p.rawImage16=get(handles.checkbox9,'Value');
+p.closeallfigure=get(handles.checkbox8,'Value');
+p.imageformat=get(handles.popupmenu3,'Value');
+p.DAPI_chal=str2double(get(handles.edit6,'String'));
+p.FISH_chal=str2double(get(handles.edit7,'String'));
+if get(handles.popupmenu1,'Value')==1;
+    p.gau_type = 'gaussian';
+else
+    p.gau_type = 'gaussian';
+end
+p.gau_hsize=str2double(get(handles.edit3,'String'));
+p.gau_sigma=str2double(get(handles.edit4,'String'));
+p.init_stack=str2double(get(handles.edit8,'String'));
+p.final_stack=str2double(get(handles.edit9,'String'));
+p.back_th_num=str2double(get(handles.edit2,'String'));
+p.back_back_erode=str2double(get(handles.edit5,'String'));
+p.back_back_open=str2double(get(handles.edit10,'String'));
+p.back_nuc_open=str2double(get(handles.edit11,'String'));
+p.back_nuc_peakthresh=str2double(get(handles.edit12,'String'));
+p.candi_start=str2double(get(handles.edit13,'String'));
+p.candi_end=str2double(get(handles.edit14,'String'));
+p.nearby_radius_max=str2double(get(handles.edit15,'String'));
+p.remove_bou_back=str2double(get(handles.edit16,'String'));
+p.dist_radius_ratio=str2double(get(handles.edit17,'String'));
+
+save([datafolder 'data.mat'],'p','-append');
+
+guidata(hObject, handles);
+%}
+varargout{1} = p;
+
+end
+
+% --- Executes when user attempts to close figure1.
+function figure1_CloseRequestFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% Update handles structure
+guidata(hObject, handles);
+load(['/functions/' 'root.mat']);
+load([datafolder 'data.mat']);
+
+p.savetiff=get(handles.checkbox1,'Value');
+p.savefinalresult=get(handles.checkbox2,'Value');
+p.rawImage16=get(handles.checkbox9,'Value');
+p.closeallfigure=get(handles.checkbox8,'Value');
+p.imageformat=get(handles.popupmenu3,'Value');
+p.DAPI_chal=str2double(get(handles.edit6,'String'));
+p.FISH_chal=str2double(get(handles.edit7,'String'));
+if get(handles.popupmenu1,'Value')==1;
+    p.gau_type = 'gaussian';
+else
+    p.gau_type = 'gaussian';
+end
+p.gau_hsize=str2double(get(handles.edit3,'String'));
+p.gau_sigma=str2double(get(handles.edit4,'String'));
+p.init_stack=str2double(get(handles.edit8,'String'));
+p.final_stack=str2double(get(handles.edit9,'String'));
+p.back_th_num=str2double(get(handles.edit2,'String'));
+p.back_back_erode=str2double(get(handles.edit5,'String'));
+p.back_back_open=str2double(get(handles.edit10,'String'));
+p.back_nuc_open=str2double(get(handles.edit11,'String'));
+p.back_nuc_peakthresh=str2double(get(handles.edit12,'String'));
+p.candi_start=str2double(get(handles.edit13,'String'));
+p.candi_end=str2double(get(handles.edit14,'String'));
+p.nearby_radius_max=str2double(get(handles.edit15,'String'));
+p.remove_bou_back=str2double(get(handles.edit16,'String'));
+p.dist_radius_ratio=str2double(get(handles.edit17,'String'));
+
+save([datafolder 'data.mat'],'p','-append');
+guidata(hObject, handles);
+varargout{1} = p;
+% Hint: delete(hObject) closes the figure
+delete(hObject);
+end
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Choose default command line output for parameter_setting
+handles.output = hObject;
+
+% Update handles structure
+load(['/functions/' 'root.mat']);
+load([datafolder 'data.mat']);
+handles.datafolder=datafolder;
+p=p_original;
+% load p parameters from data.mat in datafolder
+set(handles.checkbox1,'Value',p.savetiff);
+set(handles.checkbox2,'Value',p.savefinalresult);
+set(handles.checkbox9,'Value',p.rawImage16);
+set(handles.checkbox8,'Value',p.closeallfigure);
+set(handles.popupmenu3,'Value',p.imageformat);
+set(handles.edit6,'String',num2str(p.DAPI_chal));
+set(handles.edit7,'String',num2str(p.FISH_chal));
+if strcmp(p.gau_type,'gaussian')==1
+    set(handles.popupmenu1,'Value',1);
+else
+    set(handles.popupmenu1,'Value',1);
+end
+set(handles.edit3,'String',num2str(p.gau_hsize));
+set(handles.edit4,'String',num2str(p.gau_sigma));
+set(handles.edit8,'String',num2str(p.init_stack));
+set(handles.edit9,'String',num2str(p.final_stack));
+set(handles.edit2,'String',num2str(p.back_th_num));
+set(handles.edit5,'String',num2str(p.back_back_erode));
+set(handles.edit10,'String',num2str(p.back_back_open));
+set(handles.edit11,'String',num2str(p.back_nuc_open));
+set(handles.edit12,'String',num2str(p.back_nuc_peakthresh));
+set(handles.edit13,'String',num2str(p.candi_start));
+set(handles.edit14,'String',num2str(p.candi_end));
+set(handles.edit15,'String',num2str(p.nearby_radius_max));
+set(handles.edit16,'String',num2str(p.remove_bou_back));
+set(handles.edit17,'String',num2str(p.dist_radius_ratio));
+
+
+save([datafolder 'data.mat'],'p','-append');
+guidata(hObject, handles);
+
+
 end
 
 
@@ -524,3 +692,189 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 end
+
+
+
+function edit17_Callback(hObject, eventdata, handles)
+% hObject    handle to edit17 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit17 as text
+%        str2double(get(hObject,'String')) returns contents of edit17 as a double
+end
+
+% --- Executes during object creation, after setting all properties.
+function edit17_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit17 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+end
+
+
+function edit13_Callback(hObject, eventdata, handles)
+% hObject    handle to edit13 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit13 as text
+%        str2double(get(hObject,'String')) returns contents of edit13 as a double
+end
+
+% --- Executes during object creation, after setting all properties.
+function edit13_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit13 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+end
+
+
+function edit14_Callback(hObject, eventdata, handles)
+% hObject    handle to edit14 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit14 as text
+%        str2double(get(hObject,'String')) returns contents of edit14 as a double
+end
+
+% --- Executes during object creation, after setting all properties.
+function edit14_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit14 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+end
+
+
+function edit15_Callback(hObject, eventdata, handles)
+% hObject    handle to edit15 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit15 as text
+%        str2double(get(hObject,'String')) returns contents of edit15 as a double
+end
+
+% --- Executes during object creation, after setting all properties.
+function edit15_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit15 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+end
+
+
+function edit16_Callback(hObject, eventdata, handles)
+% hObject    handle to edit16 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit16 as text
+%        str2double(get(hObject,'String')) returns contents of edit16 as a double
+end
+
+% --- Executes during object creation, after setting all properties.
+function edit16_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit16 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+end
+
+
+function edit10_Callback(hObject, eventdata, handles)
+% hObject    handle to edit10 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit10 as text
+%        str2double(get(hObject,'String')) returns contents of edit10 as a double
+end
+
+% --- Executes during object creation, after setting all properties.
+function edit10_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit10 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+end
+
+
+function edit11_Callback(hObject, eventdata, handles)
+% hObject    handle to edit11 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit11 as text
+%        str2double(get(hObject,'String')) returns contents of edit11 as a double
+end
+
+% --- Executes during object creation, after setting all properties.
+function edit11_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit11 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+end
+
+
+function edit12_Callback(hObject, eventdata, handles)
+% hObject    handle to edit12 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit12 as text
+%        str2double(get(hObject,'String')) returns contents of edit12 as a double
+end
+
+% --- Executes during object creation, after setting all properties.
+function edit12_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit12 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+end
+
